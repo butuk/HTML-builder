27f8cd8a5ec33a0ca05223e8aaba7c091f214ccd
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-05-21 13:45:59 +0400

    feat: complete 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,12 @@
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
 
-const { stdin, stdout } = process;
-
-stdin.on('data', data => {
-  stdout.write(data);
-  process.exit();
-});
+fs.readFile(
+  path.join(__dirname, './', 'text.txt'),
+  'utf-8',
+  (err, data) => {
+    if (err) throw err;
+    stdout.write(data);
+  }
+);
