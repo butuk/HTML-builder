d1fdaed53469f6be2407605f08001e230d7b33b8
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-05-23 23:38:03 +0400

    complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const path = require('path');
+const folderPath = path.join(__dirname, './files');
+const newFolderPath = path.join(__dirname, './files-copy');
+const { unlink } = require('fs');
+const { mkdir, readdir, copyFile } = require('fs/promises');
 
+async function copyDir() {
+  await mkdir(newFolderPath, {recursive: true});
+  const oldFiles = await readdir(newFolderPath);
+  await oldFiles.forEach(file => {
+    const oldFilePath = path.join(newFolderPath, file);
+    unlink(oldFilePath, err => {
+      if(err) throw err;
+    });
+  });
+  const files = await readdir(folderPath);
+  await files.forEach(file => {
+    const filePath = path.join(folderPath, file);
+    const newFilePath = path.join(newFolderPath, file);
+    copyFile(filePath, newFilePath);
+  });
+  console.log('Folder is synced (or created and sync)!');
+}
+
+(async () => {
+  try {
+    await copyDir();
+  } catch (error) {
+    console.log(error);
+  }
+})();
