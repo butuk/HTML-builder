129d4ffbd51f1a1eaaa14b6edf32a8c3731631a9
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-05-23 16:10:26 +0400

    refactor: complete two first tasks using streams

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
+const filePath = path.join(__dirname, './', 'text.txt');
+const stream = fs.createReadStream(filePath, 'utf-8');
+let data = '';
 
-fs.readFile(
-  path.join(__dirname, './', 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    stdout.write(data);
-  }
-);
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => stdout.write(data));
+stream.on('error', error => console.log('Error', error.message));
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,37 +4,26 @@ const readline = require('readline');
 const {  stdin: input, stdout: output } = process;
 
 const rl = readline.createInterface({ input, output });
-
-fs.writeFile(
-  path.join(__dirname, './', 'text.txt'),
-  '',
-  (err) => {
-    if (err) throw err;
-  }
-);
+const filePath = path.join(__dirname, './', 'text.txt');
+const stream = fs.createWriteStream(filePath, 'utf-8');
 
 input.write('Write something please:\n');
 
 rl.on('line', (input) => {
   if(input === 'exit') {
     rl.close();
-    output.write(
-      'Thank you! Good bye!'
-    );
   } else {
-    fs.appendFile(
-      path.join(__dirname, './', 'text.txt'),
-      `${input}\n`,
-      err => {
-        if (err) throw err;
-      }
-    );
+    stream.write(`${input}\n`);
+    stream.on('error', error => console.log('Error', error.message));
   }
 });
 
+rl.on('error', error => console.log('Error', error.message));
+
 process.on('exit', () => {
-  rl.close();
   output.write(
     'Thank you! Good bye!'
   );
 });
+
+process.on('error', error => console.log('Error', error.message));
